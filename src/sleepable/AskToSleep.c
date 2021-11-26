f8c3a42c6695bd5b7f9654ed8b6866e2275306cb
jaume <jaume@westial.com>
2021-11-26 17:26:13 +0900

    PMU makes registered devices sleep #6 * Before implementing interface

--- src/sleepable/AskToSleep.c
+++ src/sleepable/AskToSleep.c
@@ -1,9 +1,15 @@
 #include "AskToSleep.h"
 #include "Sleepable.h"
 
-unsigned char AskToSleep(Sleepable aDevice, unsigned char power_level) {
-  if (aDevice.power_threshold > power_level) {
-    aDevice.sleep();
+static void make_sleep(Sleepable* aDevice);
+
+void make_sleep(Sleepable* aDevice) {
+  aDevice->sleep();
+}
+
+unsigned char AskToSleep(Sleepable *aDevice, unsigned char power_level) {
+  if (aDevice->power_threshold > power_level) {
+    make_sleep(aDevice);
     return 1;
   }
   return 0;
--- test/sleepable/AskDeviceToSleepTest.cpp
+++ test/sleepable/AskDeviceToSleepTest.cpp
@@ -2,10 +2,18 @@
 #include "CppUTest/TestHarness.h"
 #include "Sleepable.h"
 
-static void spy_simple_sleep_command();
 static int spy_sleep_counter;
 
+static void spy_simple_sleep_command();
+static void spy_two_args_sleep_command(int arg1, char *args2);
+
+// -----------------------------------------------------------------------------
+
 void spy_simple_sleep_command() {
+  ++spy_sleep_counter;
+}
+
+static void spy_two_args_sleep_command(int arg1, char *args2) {
   ++spy_sleep_counter;
 }
 
@@ -19,20 +27,26 @@ TEST_GROUP(AskToSleep){
 TEST(AskToSleep, BypassPowerCheck) {
   Sleepable device;
   device.power_threshold = 0;
-  CHECK_FALSE(AskToSleep(device, 0));
-  CHECK_FALSE(AskToSleep(device, 50));
-  CHECK_FALSE(AskToSleep(device, 100));
+  CHECK_FALSE(AskToSleep(&device, 0));
+  CHECK_FALSE(AskToSleep(&device, 50));
+  CHECK_FALSE(AskToSleep(&device, 100));
 }
 
 TEST(AskToSleep, ReturnNoNeedToSleep) {
   Sleepable device;
   device.power_threshold = 50;
-  CHECK_FALSE(AskToSleep(device, 50));
-  CHECK_FALSE(AskToSleep(device, 51));
+  CHECK_FALSE(AskToSleep(&device, 50));
+  CHECK_FALSE(AskToSleep(&device, 51));
 }
 
-TEST(AskToSleep, MakeDeviceSleep) {
+TEST(AskToSleep, MakeSimpleDeviceSleep) {
   Sleepable device = Sleepable_Create(100, spy_simple_sleep_command);
-  CHECK_TRUE(AskToSleep(device, 99));
+  CHECK_TRUE(AskToSleep(&device, 99));
   CHECK_EQUAL(1, spy_sleep_counter);
 }
+
+//TEST(AskToSleep, MakeSleepADeviceWitTwoArgsCallback) {
+//  Sleepable device = Sleepable_Create(100, spy_two_args_sleep_command);
+//  CHECK_TRUE(AskToSleep(&device, 99));
+//  CHECK_EQUAL(1, spy_sleep_counter);
+//}
